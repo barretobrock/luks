cecd28f5944b94048417961ea332cad1546106d3
bobrock <barret@tarkmail.com>
2021-10-31 10:44:12 -0500

    Relocate app instance in wsgi out of __main__ test

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,10 @@ from luks import ProductionConfig
 from luks.api import create_app
 
 
+log = LogWithInflux('luks-wsgi')
+app = create_app(config_class=ProductionConfig)
+
+
 if __name__ == '__main__':
     # Instantiate log here, as the hosts API is requested to communicate with influx
-    log = LogWithInflux('luks-api')
-    config = ProductionConfig
-    app = create_app(config_class=config)
     app.run()
