52cf29fe0da2326a7efb153635feea21ef90c3b3
barret <barret@tarkmail.com>
2022-06-25 16:01:59 -0500

    Add gunicorn to reqs, move app to be visible

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,8 @@ from pukr import get_logger
 from luks.config import ProductionConfig
 from luks.api import create_app
 
+app = create_app(config_class=ProductionConfig)
 
 if __name__ == '__main__':
     log = get_logger(log_name='luks-api', log_dir_path=pathlib.Path().home().joinpath('logs/luks'))
-    app = create_app(config_class=ProductionConfig)
     app.run(host=ProductionConfig.DB_SERVER, port=ProductionConfig.PORT)
